1c020096093ee288c4025e1f0f1875c03a875d34
Oleg <legyss@mail.ru>
2023-02-01 13:24:55 +0600

    Added Task_43

---
+++ Task_41/Program.cs
@@ -0,0 +1,35 @@
+﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. 
+Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3 */
+int[] EnteringArray()
+{
+    Console.WriteLine("Введите размер массива: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[n];
+    Console.WriteLine("Заполните массив.");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"array[{i}] = ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine("Массив заполнен.");
+    return array;
+}
+
+int CountingNumberOfLargeZeros(int[] a)
+{
+    int count = 0;
+    for (int i = 0; i < a.Length; i++)
+    {
+        if (a[i] > 0) count++;
+    }
+    return count;
+
+}
+
+int[] array = EnteringArray();
+Console.WriteLine("************************************************************");
+Console.WriteLine("Во веденном массиве количество чисел больших нуля, следующее");
+Console.WriteLine(String.Join(", ", array)
+                    + $" -> {CountingNumberOfLargeZeros(array)}");
---
+++ Task_43/Program.cs
@@ -0,0 +1,54 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения 
+двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+Console.WriteLine("Введите коэффициенты двух линейных уравнений");
+Console.Write("b1 = ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k1 = ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("b2 = ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("k2 = ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+string SearchIntersectionOfStraightLines(int k1, int b1, int k2, int b2)
+{
+    //Расчет будем проводить в декартовой системе координат
+    //для расчетов необходимо дополнительно задать область
+    //в которой будем рассматривать прямые 
+    double xMin = -100;
+    double xMax = 100;
+    // и шаг с которым будем проводить расчеты
+    double step = 0.01;
+    double y1 = 0.0;
+    double y2 = 0.0;
+    string str = "Значение не найдено!";
+    for (double x = xMin; x <= xMax; x += step)
+    {
+        x = Math.Round(x, 3); // округления x использую для красоты вывода
+        // округление  y1 и y2 используется в связи стем,
+        // что мы в дальнейшем их сравниваем, а числа  
+        // формата double хоть на десятый знак поле запятой, но отличаются!
+        y1 = Math.Round(k1 * x + b1, 3);
+        y2 = Math.Round(k2 * x + b2, 3);
+        
+        // тест расчетов
+        // Console.Write("x = ");
+        // Console.Write(x);
+        // Console.Write("   y1 = ");
+        // Console.Write(y1);
+        // Console.Write("   y2 = ");
+        // Console.WriteLine(y2);
+
+        if (y1 == y2)
+        {
+            str = $"Прямые пересекаются в точке с координатами ({x}; {y1}).";
+            break;
+        }
+    }
+    return str;
+}
+
+Console.WriteLine(SearchIntersectionOfStraightLines(k1, b1, k2, b2));
